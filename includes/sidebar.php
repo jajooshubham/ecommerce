8a9a219e7082fa5c2547c4c97a7b5226e5af81f1
jajooshubham <59220613+jajooshubham@users.noreply.github.com>
2020-06-02 00:23:17 +0530

    shop page done

---
+++ includes/sidebar.php
@@ -0,0 +1,28 @@
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading">
+		<h3 class="panel-title">Product Categories</h3>
+	</div>
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			<li><a href="shop.php">Jackets</a></li>
+			<li><a href="shop.php">Acessories</a></li>
+			<li><a href="shop.php">Shoes</a></li>
+			<li><a href="shop.php">Coats</a></li>
+			<li><a href="shop.php">T-shirts</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading">
+		<h3 class="panel-title">Categories</h3>
+	</div>
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			<li><a href="shop.php">Mens</a></li>
+			<li><a href="shop.php">Womens</a></li>
+			<li><a href="shop.php">Kids</a></li>
+			<li><a href="shop.php">Others</a></li>
+		</ul>
+	</div>
+</div>
--- shop.php
+++ shop.php
@@ -166,6 +166,211 @@
 					<li>Shop</li>
 				</ul>
 			</div>
+
+			<div class="col-md-3">
+				<?php
+				include("includes/sidebar.php");
+				?>
+			</div>
+			<div class="col-md-9">
+				<div class="box">
+					<h1>Shop</h1>
+					<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-4 col-sm-6 single center-responsive">
+						<div class="product">
+							<a href="details.php">
+								<img src="admin_area/product_images/bg_1.jpg" class="img-responsive">
+							</a>
+							<div class="text">
+								<h3>
+									<a href="details.php">BENETTON White polo shirt</a>
+								</h3>
+								<p class="price"> INR 299</p>
+								<p class="buttons">
+									<a href="details.php" class="btn btn-default">View Details</a>
+									<a href="details.php" class="btn btn-primary">
+										<i class="fa fa-shopping-cart"></i>  Add To Cart
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<center>
+					<ul class="pagination">
+						<li><a href="">First Page</a></li>
+						<li><a href="">2</a></li>
+						<li><a href="">3</a></li>
+						<li><a href="">4</a></li>
+						<li><a href="">5</a></li>
+						<li><a href="">Last Page</a></li>
+					</ul>
+				</center>
+			</div>
 		</div>
 	</div>
 
