a650fe66458e7ae13f044a26464dc5a7bfd61bf2
abhishrimal <60340221+abhishrimal@users.noreply.github.com>
2020-06-03 13:57:06 +0530

    Add files via upload

---
+++ admin_area/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/npm.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<title>E-Commerce Store | Sec2Hand</title>
+</head>
+<body>
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<!-- footer -->
+	<?php
+	include("includes/footer.php");
+	?>
+
+</body>
+</html>
