a2c5b58c10d9071380529ebf82dec4d0be444099
Priyanshu Chouhan <priyanshu.chouhan11.pc@gmail.com>
2020-06-13 19:51:04 +0530

    abc

---
+++ admin_area/insert_product.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert Product</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/npm.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+</body>
+</html>
